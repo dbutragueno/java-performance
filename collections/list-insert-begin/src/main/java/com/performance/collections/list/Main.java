2181881710c9f0ee9bf7af19e62aeb2ddfed6b8e
davidbutragueno <david.butra23@gmail.com>
2026-05-10 13:13:59 +0200

    test(java-performance): Add JMH benchmarks for array list initial capacity

---
+++ collections/list-insert-begin/src/main/java/com/performance/collections/list/Main.java
@@ -0,0 +1,52 @@
+package com.performance.collections.list;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+@State(Scope.Thread)
+public class Main {
+
+    private static final int INSERT_ITERATIONS = 1000000;
+
+    private final List<Integer> arrayList = new ArrayList<>();
+    private final List<Integer> linkedList = new LinkedList<>();
+
+    void main() throws Exception {
+        String[] args = new String[0];
+        org.openjdk.jmh.Main.main(args);
+
+        insertBeginArrayList();
+        insertBeginLinkedList();
+    }
+
+    @Setup
+    public void clear() {
+        arrayList.clear();
+        linkedList.clear();
+    }
+
+    @Benchmark
+    @Fork(value = 1, warmups = 1)
+    @Warmup(iterations = 1)
+    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
+    public void insertBeginArrayList() {
+        insetBegin(arrayList);
+    }
+
+    @Benchmark
+    @Fork(value = 1, warmups = 1)
+    @Warmup(iterations = 1)
+    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
+    public void insertBeginLinkedList() {
+        insetBegin(linkedList);
+    }
+
+    private void insetBegin(List<Integer> list) {
+        for (int i = 0; i < INSERT_ITERATIONS; i++) {
+            list.addFirst(i);
+        }
+    }
+}
